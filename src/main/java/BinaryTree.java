2c5a7ea1a1a378ad422cca2c471d5c15dc3875a4
Emil <emil93@gmail.com>
2017-09-23 10:31:29 +0200

    Set up the skeleton for a Driver and a Binary Tree class

---
+++ src/main/java/BinaryTree.java
@@ -0,0 +1,26 @@
+import java.util.Iterator;
+
+public class BinaryTree<Key> implements Iterable {
+
+    public BinaryTree() {
+    }
+
+    public Key get() {
+        return null;
+    }
+
+    public int count() {
+        return 0;
+    }
+
+    public int distinct() {
+        return 0;
+    }
+
+    public Iterator iterator() {
+
+        return null;
+    }
+
+    private class
+}
