8bf22872a36853848a638d9933e09075cf243a7c
Emil <emil93@gmail.com>
2017-09-24 15:03:14 +0200

    Began working on the put() method in BinaryTree

--- src/main/java/BinaryTree.java
+++ src/main/java/BinaryTree.java
@@ -1,11 +1,32 @@
 import java.util.Iterator;
 
-public class BinaryTree<Key> implements Iterable {
+public class BinaryTree implements Iterable {
+
+    private final static int NUMBER_OF_BRANCHES = 256; // EXTENDED ASCII
+
+    private Node root;
 
     public BinaryTree() {
+        this.root = new Node(" ");
     }
 
-    public Key get() {
+    public void put(String key) {
+        Node node = root;
+
+        for (int i = 0; i < key.length(); i++) {
+            int currentChar = key.charAt(i);
+            if (node.nodes[currentChar] != null) {
+                node = node.nodes[currentChar];
+                node.value++;
+            } else {
+                node.nodes[currentChar] = new Node();
+                node = node.nodes[currentChar];
+                node.value = 1;
+            }
+        }
+    }
+
+    public String get() {
         return null;
     }
 
@@ -22,5 +43,33 @@ public class BinaryTree<Key> implements Iterable {
         return null;
     }
 
-    private class
+    private class Node {
+
+        String key;
+        int value = 0;
+
+        Node[] nodes = new Node[NUMBER_OF_BRANCHES]; //TODO
+
+        public Node() {
+        }
+
+        public Node(String key) {
+            this.key = key;
+        }
+    }
+
+    private class BinaryTreeIterator implements Iterator<BinaryTree> {
+
+        public boolean hasNext() {
+            return false;
+        }
+
+        public BinaryTree next() {
+            return null;
+        }
+
+        public void remove() {
+            /* No implementation */
+        }
+    }
 }
--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -3,5 +3,15 @@ public class Driver {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
+
+        /*
+        If str1 precedes str2, the return key is -1.
+        If it's the other way around, the return key is 1.
+        If they are equal, the return key is 0.
+         */
+        String str1 = "a";
+        String str2 = "b";
+        System.out.println(str1.compareTo(str2));
     }
+
 }
